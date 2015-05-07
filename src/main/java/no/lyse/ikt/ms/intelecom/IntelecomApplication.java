79acb0b5d8c3c76c4ee8dca56b88938535f69f89
Henrik W Lund <henrik.w.lund@gmail.com>
2015-05-07 15:23:09 +0200

    Initial commit

---
+++ src/main/java/no/lyse/ikt/ms/intelecom/IntelecomApplication.java
@@ -0,0 +1,12 @@
+package no.lyse.ikt.ms.intelecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelecomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelecomApplication.class, args);
+    }
+}

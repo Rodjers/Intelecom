ab69fe519cbefd10dcef1ac5d9aef585bb11087f
Oddgeir Gitlestad <oddgeir.gitlestad@gitlestadit.no>
2015-05-11 11:34:13 +0200

    Implemented request add transformer request add callout and response transformer

--- src/main/java/no/lyse/ikt/ms/intelecom/no/lyse/ikt/ms/intelecom/config/Infrastructure.java
+++ src/main/java/no/lyse/ikt/ms/intelecom/no/lyse/ikt/ms/intelecom/config/Infrastructure.java
@@ -7,15 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.integration.transformer.ContentEnricher;
 import org.springframework.integration.transformer.GenericTransformer;
 import org.springframework.integration.ws.MarshallingWebServiceInboundGateway;
 import org.springframework.integration.ws.MarshallingWebServiceOutboundGateway;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.oxm.Marshaller;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.config.annotation.EnableWs;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Result;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -78,6 +82,15 @@ public class Infrastructure {
     }
 
     @Bean
+    MarshallingWebServiceOutboundGateway requestAddGateway() {
+        MarshallingWebServiceOutboundGateway authenticateGateway = new MarshallingWebServiceOutboundGateway(
+                "https://api.intele.com/Connect/ContactCentre/Agent.svc", jaxb2Marshaller(), jaxb2Marshaller()
+        );
+
+        return authenticateGateway;
+    }
+
+    @Bean
     GenericTransformer<AuthenticateResponse,Map<String,String>> authenticateReplyTransformer() {
         return new GenericTransformer<AuthenticateResponse, Map<String, String>>() {
             @Override
@@ -91,26 +104,88 @@ public class Infrastructure {
     }
 
     @Bean
-    GenericTransformer<Map<String,String>, Message<RequestAdd>> requestAddTransformer() {
-        return new GenericTransformer<Map<String,String>, Message<RequestAdd>>() {
+    GenericTransformer<Message<Map<String,String>>, Message<RequestAdd>> requestAddTransformer() {
+        return new GenericTransformer<Message<Map<String,String>>, Message<RequestAdd>>() {
             @Override
-            public Message<RequestAdd> transform(Map<String,String> source) {
+            public Message<RequestAdd> transform(Message<Map<String,String>> messageSource) {
+
+                Map<String,String> source = messageSource.getPayload();
                 ObjectFactory factory = new ObjectFactory();
                 RequestAdd requestAdd = factory.createRequestAdd();
 
                 requestAdd.setAccessToken(factory.createRequestAddAccessToken(source.get("accesstoken")));
                 requestAdd.setCustomerKey(factory.createRequestAddCustomerKey(source.get("customerKey")));
 
-                InfoType infoXml = factory.createInfoType();
-                infoXml.setFrom(source.get("from"));
-                infoXml.setReceivedTime(source.get("receivedTime"));
-                infoXml.setSentTime(source.get("sentTime"));
-                infoXml.setSubject(source.get("subject"));
-                infoXml.setTo(source.get("to"));
-                infoXml.setUri("uri");
+                StringBuilder sb = new StringBuilder();
 
+                sb.append("&lt;info>");
+                sb.append("&lt;from>" + source.get("from") + sb.append("</from>"));
+                sb.append("&lt;to>" + source.get("to") + sb.append("</to>"));
+                sb.append("&lt;sentTime>" + source.get("sentTime") + sb.append("</sentTime>"));
+                sb.append("&lt;receivedTime>" + source.get("receivedTime") + sb.append("</receivedTime>"));
+                sb.append("&lt;subject>" + source.get("subject") + sb.append("</subject>"));
+                sb.append("&lt;uri>" + source.get("uri") + sb.append("</uri>"));
+                sb.append("</info>");
 
-                return null;
+                requestAdd.setInfoXml(factory.createRequestAddInfoXml(sb.toString()));
+
+                MessageBuilder mb = MessageBuilder.withPayload(requestAdd);
+                mb.setHeader("RequestId", source.get("requestId"));
+                mb.setHeader("ws_soapAction", "ContactCentreWebServices/IAgent/RequestAdd");
+
+                return mb.copyHeadersIfAbsent(messageSource.getHeaders()).build();
+            }
+        };
+    }
+
+    @Bean
+    GenericTransformer<Object, Object> debugTransformer() {
+        return new GenericTransformer<Object, Object>() {
+            @Override
+            public Object transform(Object source) {
+
+                ObjectFactory factory = new ObjectFactory();
+
+                InboundRequestAddResponse inboundRequestAddResponse = factory.createInboundRequestAddResponse();
+                JAXBElement<ResponseType> jaxbResponseType = inboundRequestAddResponse.getRequestAddResult();
+
+                ResponseType responseType = jaxbResponseType.getValue();
+
+        //        responseType.setCode(source.getPayload().getRequestAddResult().getValue().getResult());
+     //           responseType.setRequestId((String) source.getHeaders().get("RequestId"));
+
+                jaxbResponseType.setValue(responseType);
+                inboundRequestAddResponse.setRequestAddResult(jaxbResponseType);
+
+                MessageBuilder<InboundRequestAddResponse> mb = MessageBuilder.withPayload(inboundRequestAddResponse);
+
+                return mb.build();
+            }
+        };
+    }
+
+    @Bean
+    GenericTransformer<Message<RequestAddResponse>,Message<InboundRequestAddResponse>> requestAddReplyTransformer() {
+        return new GenericTransformer<Message<RequestAddResponse>, Message<InboundRequestAddResponse>>() {
+            @Override
+            public Message<InboundRequestAddResponse> transform(Message<RequestAddResponse> source) {
+
+                ObjectFactory factory = new ObjectFactory();
+
+                InboundRequestAddResponse inboundRequestAddResponse = factory.createInboundRequestAddResponse();
+
+                ResponseType responseType = factory.createResponseType();
+                responseType.setCode(source.getPayload().getRequestAddResult().getValue().getResult());
+                responseType.setRequestId((String) source.getHeaders().get("RequestId"));
+
+                QName responsTypeQName = new QName("http://www.example.org/IntelecomRequestAdd", "ResponseType");
+                JAXBElement<ResponseType> responseTypeJAXBElement = new JAXBElement<ResponseType>(responsTypeQName, ResponseType.class, responseType);
+
+                inboundRequestAddResponse.setRequestAddResult(responseTypeJAXBElement);
+
+                MessageBuilder<InboundRequestAddResponse> mb = MessageBuilder.withPayload(inboundRequestAddResponse);
+
+                return mb.copyHeadersIfAbsent(source.getHeaders()).build();
             }
         };
     }
